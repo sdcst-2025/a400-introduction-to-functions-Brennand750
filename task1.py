f9ac79cc767cbde710fcfb8d02fbdf77291a92f7
Brennand750 <brennand750@deltalearns.ca>
2026-03-30 10:15:26 -0700

    Brennan Introduction To Functions

--- task1.py
+++ task1.py
@@ -9,7 +9,21 @@ Execute the function of the letter they use.
 """
 
 def A():
+    print("Hello")
 
 def B():
+    print("How are you")
 
 def C():
+    print("Hi")
+
+user_input = input("Please enter a letter from A to C")
+
+if user_input == 'a':
+    A()
+elif user_input == 'b':
+    B()
+elif user_input == 'c':
+    C()
+    
+
--- task2.py
+++ task2.py
@@ -10,3 +10,29 @@ plays the game
 This will be silimar to something you have already done, but in this task you 
 are breaking the code up into different sections to make each a function.
 """
+import random
+def title():
+    print("the main goal of the game is to guess a random number that generated from 1 to 100 in the least amount of guesses. / Also the game will tell you if your guess is too high or too low.")
+
+def game():
+    rnum = random.randint(1,100)
+    attempts = 0
+    print("Please guess a random number")
+    while True:
+        guess = input("Please guess a random number from 1 to 100")
+        guess = int(guess)
+        attempts += 1
+        if guess > rnum:
+            print("Your guess is too high")
+        elif guess < rnum:
+            print("Your guess is too low")
+        else:
+            print("You are correct")
+            print("it took you this many trys", attempts)
+            break
+
+if __name__ == "__main__":
+    title()
+    game()
+        
+
